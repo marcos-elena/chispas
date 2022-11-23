897f5d51a28f332ad010440ffb1f3cd1cab5d57f
Marcos Elena San Segundo <marcos.elesan@educa.jcyl.es>
2022-11-23 15:51:38 +0100

    Creación de la interfaz clientes y las clases autonomo y sociedad

---
+++ src/Autonomo.java
@@ -0,0 +1,43 @@
+public class Autonomo implements Clientes {
+
+    private String id;
+    private String age;
+
+    public void setId(String id) {
+        this.id = id;
+    }
+    @Override
+    public String getId() {
+        return id;
+    }
+
+    @Override
+    public String getDni() {
+        return null;
+    }
+
+    @Override
+    public String getDireccionPostal() {
+        return null;
+    }
+
+    @Override
+    public String getPoblacion() {
+        return null;
+    }
+
+    @Override
+    public String getProvincia() {
+        return null;
+    }
+
+    @Override
+    public String getEmail() {
+        return null;
+    }
+
+    @Override
+    public String getTelefono() {
+        return null;
+    }
+}
---
+++ src/Clientes.java
@@ -0,0 +1,9 @@
+public interface Clientes {
+    public String getId();
+    public String getDni();
+    public String getDireccionPostal();
+    public String getPoblacion();
+    public String getProvincia();
+    public String getEmail();
+    public String getTelefono();
+}
