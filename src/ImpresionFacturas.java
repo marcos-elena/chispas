08a07ea361c043cefd64deebd00f40fa0a10030f
Marcos Elena San Segundo <marcos.elesan@educa.jcyl.es>
2022-11-24 16:11:12 +0100

    Creación de las clases facturas e impresionFacturas

---
+++ src/ImpresionFacturas.java
@@ -0,0 +1,56 @@
+public class ImpresionFacturas {
+    private Integer id;
+    private String fecha;
+    private Clientes cliente;
+    private Ventas venta;
+    private Integer baseImponible;
+    private Integer total;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(String fecha) {
+        this.fecha = fecha;
+    }
+
+    public Clientes getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(Clientes cliente) {
+        this.cliente = cliente;
+    }
+
+    public Ventas getVenta() {
+        return venta;
+    }
+
+    public void setVenta(Ventas venta) {
+        this.venta = venta;
+    }
+
+    public Integer getBaseImponible() {
+        return baseImponible;
+    }
+
+    public void setBaseImponible(Integer baseImponible) {
+        this.baseImponible = baseImponible;
+    }
+
+    public Integer getTotal() {
+        return total;
+    }
+
+    public void setTotal(Integer total) {
+        this.total = total;
+    }
+}
